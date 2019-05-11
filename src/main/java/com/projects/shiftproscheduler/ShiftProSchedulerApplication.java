302c3dbc14b9bbd5b93f22d19247d68d2c52f0b9
Jack <wjbz82@gmail.com>
2019-05-10 21:25:06 -0700

    changing repo structure to spring / gradle

---
+++ src/main/java/com/projects/shiftproscheduler/ShiftProSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.projects.shiftproscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftProSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftProSchedulerApplication.class, args);
+	}
+
+}
