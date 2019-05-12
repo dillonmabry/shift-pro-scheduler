12c67af9e075ce1ab1d37d3be49a8085b2247f1b
Jack <wjbz82@gmail.com>
2019-05-11 22:57:52 -0700

    adding actual files to repo

--- src/main/java/com/projects/shiftproscheduler/Controller/Controller.java
+++ src/main/java/com/projects/shiftproscheduler/Controller/Controller.java
@@ -1,4 +1,13 @@
 package com.projects.shiftproscheduler.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
 public class Controller {
+
+    @RequestMapping(value = "/")
+    public String index(){
+        return "index";
+    }
+
 }
