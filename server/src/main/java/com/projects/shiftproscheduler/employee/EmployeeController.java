699adb6f8e0e8a6cc0653568bc89f5a54eb85e50
dillonmabry <dcmabry@gmail.com>
2019-05-12 17:18:44 -0400

    Add maven support for build, add travis CI, add sample Spring configuration with employees entity, remove unnecessary things for React app

---
+++ server/src/main/java/com/projects/shiftproscheduler/employee/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.projects.shiftproscheduler.employee;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class EmployeeController {
+
+    private final EmployeeRepository employees;
+
+    public EmployeeController(EmployeeRepository employeeService) {
+        this.employees = employeeService;
+    }
+
+    @GetMapping({ "/employees" })
+    public @ResponseBody Employees getEmployees() {
+        Employees employees = new Employees();
+        employees.getEmployeeList().addAll(this.employees.findAll());
+        return employees;
+    }
+
+}
