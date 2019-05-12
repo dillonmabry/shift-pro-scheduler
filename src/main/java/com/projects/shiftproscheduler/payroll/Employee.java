ac59f848b3d447235c78801cb23039a3864b6092
Jack <wjbz82@gmail.com>
2019-05-11 17:39:38 -0700

    starting to incorporate react to spring app

---
+++ src/main/java/com/projects/shiftproscheduler/payroll/Employee.java
@@ -0,0 +1,4 @@
+package com.projects.shiftproscheduler.payroll;
+
+public class Employee {
+}
--- src/main/java/com/projects/shiftproscheduler/payroll/ShiftProSchedulerApplication.java
+++ src/main/java/com/projects/shiftproscheduler/payroll/ShiftProSchedulerApplication.java
@@ -1,8 +1,11 @@
 package com.projects.shiftproscheduler;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class ShiftProSchedulerApplication {
 
