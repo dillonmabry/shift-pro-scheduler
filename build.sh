1e17f55477a5462698193245f1813b9f597acc5c
Dillon Mabry <dcmabry@gmail.com>
2021-05-05 21:21:57 -0400

    Travis build works now but Xenial/Ubuntu version doesn't have required GLIBCXX version, fun stuff

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
+apt-get install libstdc++6 # Install for GLIBCXX requirements for optimizer
 cd server
-rm ~/.m2/settings.xml
-curl -o ~/.m2/settings.xml
-./mvnw dependency:go-offline
+rm ~/.m2/settings.xml # Remove default Maven settings since Travis loves to overwrite this
 ./mvnw package -DskipTests
 ./mvnw clean test jacoco:report coveralls:report
